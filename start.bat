013b49ac516e3f72d66fb1c49c9b698af6b37aa2
Samarth Prajapati <samarthp475@gmail.com>
2024-08-29 16:17:16 +0530

    updated

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem Start the frontend application
-start cmd /k "cd my-app && npm run dev"
-
-rem Start the backend application
-start cmd /k "call .venv\Scripts\activate.bat && cd ddbackend && python manage.py runserver"
